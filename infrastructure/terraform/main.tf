b3e29fff001d87d716f1ef41bdddf4723b2c15a3
Nor Shukri <shukri@sodrama.sg>
2024-06-04 21:08:09 +0800

    review Terraform files

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -2,57 +2,21 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Create VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "group-3-vpc-${var.branch_name}"
-  }
-}
-
-# Create Subnet
-resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = "group-3-subnet-${var.branch_name}"
-  }
-}
-
-# Create Security Group
-resource "aws_security_group" "main" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "group-3-sg-${var.branch_name}"
-  }
+variable "branch_name" {
+  description = "The branch name to include in the resource names"
+  type        = string
 }
 
 resource "aws_ecr_repository" "netflix_clone" {
-  name = "Group-3-ecr-repo-${var.branch_name}"
+  name = "group-3-ecr-repo-${var.branch_name}"
 }
 
 resource "aws_ecs_cluster" "netflix_clone_cluster" {
-  name = "Group-3-ecs-cluster-${var.branch_name}"
+  name = "group-3-ecs-cluster-${var.branch_name}"
 }
 
 resource "aws_ecs_task_definition" "netflix_clone_task" {
-  family                   = "Group-3-ecs-task-${var.branch_name}"
+  family                   = "group-3-ecs-task-${var.branch_name}"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
@@ -75,13 +39,40 @@ resource "aws_ecs_task_definition" "netflix_clone_task" {
 }
 
 resource "aws_ecs_service" "netflix_clone_service" {
-  name            = "Group-3-ecs-service-${var.branch_name}"
+  name            = "group-3-ecs-service-${var.branch_name}"
   cluster         = aws_ecs_cluster.netflix_clone_cluster.id
   task_definition = aws_ecs_task_definition.netflix_clone_task.arn
   desired_count   = 1
   launch_type     = "FARGATE"
   network_configuration {
-    subnets         = [aws_subnet.main.id]
-    security_groups = [aws_security_group.main.id]
+    subnets         = [aws_subnet.default.id]
+    security_groups = [aws_security_group.default.id]
+  }
+}
+
+resource "aws_vpc" "netflix_clone_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "netflix_clone_subnet" {
+  vpc_id     = aws_vpc.netflix_clone_vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "netflix_clone_sg" {
+  vpc_id = aws_vpc.netflix_clone_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
