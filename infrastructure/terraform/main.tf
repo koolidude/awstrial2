3661af0d0f0276c062a5a843b83502d58263b1ae
Nor Shukri <shukri@sodrama.sg>
2024-06-04 21:24:59 +0800

    Add IAM Role Policy

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -34,6 +34,28 @@ resource "aws_security_group" "netflix_clone_sg" {
   }
 }
 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "group-3-ecs-task-execution-role-${var.branch_name}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecr_repository" "netflix_clone" {
   name = "group-3-ecr-repo-${var.branch_name}"
 }
@@ -48,6 +70,7 @@ resource "aws_ecs_task_definition" "netflix_clone_task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   container_definitions = jsonencode([
     {
       name      = "backend"
