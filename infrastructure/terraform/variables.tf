dfcd846f195fe6dbd4290ecb714287364f44e5c4
Nor Shukri <shukri@sodrama.sg>
2024-06-04 20:48:23 +0800

    removed redundant variables and updated destroy workflow

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -4,16 +4,6 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "subnet_id" {
-  description = "The ID of the subnet to deploy to"
-  type        = string
-}
-
-variable "security_group_id" {
-  description = "The ID of the security group"
-  type        = string
-}
-
 variable "branch_name" {
   description = "The branch name to include in the resource names"
   type        = string
