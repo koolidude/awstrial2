3cac37ab8acaa77b288dde6c3b45bd257218dd8f
Nor Shukri <shukri@sodrama.sg>
2024-06-06 06:03:26 +0800

    added ALB in Terraform config

--- infrastructure/terraform/backend/main.tf
+++ infrastructure/terraform/backend/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Declare the VPC resource
+# VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
@@ -11,7 +11,7 @@ resource "aws_vpc" "main" {
   }
 }
 
-# Declare the subnet resource
+# Subnet
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
@@ -21,7 +21,7 @@ resource "aws_subnet" "main" {
   }
 }
 
-# Declare the security group resource
+# Security Group
 resource "aws_security_group" "main" {
   vpc_id = aws_vpc.main.id
 
@@ -40,25 +40,47 @@ resource "aws_security_group" "main" {
   }
 }
 
-# VPC Endpoint for ECR
-resource "aws_vpc_endpoint" "ecr" {
-  vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.us-east-1.ecr.api"
-  subnet_ids        = [aws_subnet.main.id]
-  security_group_ids = [aws_security_group.main.id]
-  vpc_endpoint_type = "Interface"
+# ALB
+resource "aws_lb" "main" {
+  name               = "group-3-alb-${var.branch_name}"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.main.id]
+  subnets            = [aws_subnet.main.id]
+
+  enable_deletion_protection = false
 }
 
-# VPC Endpoint for ECR Docker
-resource "aws_vpc_endpoint" "ecr_dkr" {
-  vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.us-east-1.ecr.dkr"
-  subnet_ids        = [aws_subnet.main.id]
-  security_group_ids = [aws_security_group.main.id]
-  vpc_endpoint_type = "Interface"
+# ALB Target Group
+resource "aws_lb_target_group" "main" {
+  name     = "group-3-tg-${var.branch_name}"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  target_type = "ip"
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200-399"
+  }
 }
 
-# IAM role for ECS task execution
+# ALB Listener
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
+
+# ECS Task Execution Role
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "group-3-ecs-task-execution-role-${var.branch_name}"
 
@@ -76,23 +98,23 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   })
 }
 
-# IAM policy attachment for ECS task execution role
+# ECS Task Execution Policy
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-# ECR repository for the application
+# ECR Repository
 resource "aws_ecr_repository" "netflix_clone" {
   name = "group-3-ecr-repo-${var.branch_name}"
 }
 
-# ECS cluster
+# ECS Cluster
 resource "aws_ecs_cluster" "netflix_clone_cluster" {
   name = "group-3-ecs-cluster-${var.branch_name}"
 }
 
-# ECS task definition
+# ECS Task Definition
 resource "aws_ecs_task_definition" "netflix_clone_task" {
   family                   = "group-3-ecs-task-${var.branch_name}"
   network_mode             = "awsvpc"
@@ -117,7 +139,7 @@ resource "aws_ecs_task_definition" "netflix_clone_task" {
   ])
 }
 
-# ECS service
+# ECS Service
 resource "aws_ecs_service" "netflix_clone_service" {
   name            = "group-3-ecs-service-${var.branch_name}"
   cluster         = aws_ecs_cluster.netflix_clone_cluster.id
@@ -128,5 +150,31 @@ resource "aws_ecs_service" "netflix_clone_service" {
     subnets         = [aws_subnet.main.id]
     security_groups = [aws_security_group.main.id]
   }
+  load_balancer {
+    target_group_arn = aws_lb_target_group.main.arn
+    container_name   = "backend"
+    container_port   = 80
+  }
+  depends_on = [
+    aws_lb_listener.main
+  ]
+}
+
+# VPC Endpoint for ECR
+resource "aws_vpc_endpoint" "ecr" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.us-east-1.ecr.api"
+  subnet_ids        = [aws_subnet.main.id]
+  security_group_ids = [aws_security_group.main.id]
+  vpc_endpoint_type = "Interface"
+}
+
+# VPC Endpoint for ECR Docker
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.us-east-1.ecr.dkr"
+  subnet_ids        = [aws_subnet.main.id]
+  security_group_ids = [aws_security_group.main.id]
+  vpc_endpoint_type = "Interface"
 }
 
