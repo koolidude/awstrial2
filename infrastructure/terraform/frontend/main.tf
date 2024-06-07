d960b0655fc0e4301b7989edd26b174343a637bd
Nor Shukri <shukri@sodrama.sg>
2024-06-08 01:44:50 +0800

    Added S3 Bucket Policy and added port 5000 in App.js

--- infrastructure/terraform/frontend/main.tf
+++ infrastructure/terraform/frontend/main.tf
@@ -18,6 +18,24 @@ resource "aws_s3_object" "frontend" {
   source = "frontend/build/${each.value}"
 }
 
+resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
+  bucket = aws_s3_bucket.frontend.bucket
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid: "PublicReadGetObject",
+        Effect = "Allow",
+        Principal: "*",
+        Action: "s3:GetObject",
+        Resource: "arn:aws:s3:::${aws_s3_bucket.frontend.bucket}/*"
+      }
+    ]
+  })
+}
+
+
 resource "aws_cloudfront_distribution" "frontend" {
   origin {
     domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
