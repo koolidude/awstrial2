b87be76b296c15ecf6e0791a9696192a2eeec4b2
meezee91 <muhammadtarmizisulaiman@gmail.com>
2024-06-09 22:10:47 +0800

    running workflow from feature

--- infrastructure/terraform/backend/main.tf
+++ infrastructure/terraform/backend/main.tf
@@ -395,3 +395,5 @@ resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
+
+#test
