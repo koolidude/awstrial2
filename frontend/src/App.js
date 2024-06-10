b7d57527868a775382d8c432832beec242f584da
meezee91 <muhammadtarmizisulaiman@gmail.com>
2024-06-10 18:15:58 +0800

    running cicd workflow for frontend from feature-tarmizi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Container, Typography, Grid, TextField, Button } from '@mui/material';
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';  // Ensure this is imported
