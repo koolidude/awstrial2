6ed914c7039901c14e184b7f4e900035824e4a27
Nor Shukri <shukri@sodrama.sg>
2024-06-11 11:55:29 +0800

    Added destroy workflow

--- backend/src/config.py
+++ backend/src/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', '123')
-    TMDB_API_KEY = os.getenv('15c9a39e65b8d2031035ea322b3e2880')
+    TMDB_API_KEY = os.getenv('TMDB_API_KEY')
     DEBUG = os.getenv('DEBUG', 'True').lower() in ('true', '1', 't')
 
 config = Config()
