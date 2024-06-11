da27731f089da39f84c959f1d34777c965a95d3a
Nor Shukri <shukri@sodrama.sg>
2024-06-11 14:13:09 +0800

    rerun cicd frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,14 @@ import './App.css';
 
 function App() {
     const [movies, setMovies] = useState([]);
-    const branchName = process.env.REACT_APP_BRANCH_NAME;
-    const backendUrl = `https://group-3-backend-${branchName}.sctp-sandbox.com:5000`;
 
     useEffect(() => {
+        const backendUrl = process.env.REACT_APP_BACKEND_URL;
         fetch(`${backendUrl}/movies`)
             .then(response => response.json())
             .then(data => setMovies(data.results))
             .catch(error => console.error('Error fetching movies:', error));
-    }, [backendUrl]);
+    }, []);
 
     return (
         <div className="App">
