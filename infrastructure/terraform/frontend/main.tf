8f094950b63f764457fecd9f4a2d3b7857483017
Nor Shukri <shukri@sodrama.sg>
2024-06-12 11:02:31 +0800

    Added Fetch Cloudfront OAI IAM ARN in CICD

--- infrastructure/terraform/frontend/main.tf
+++ infrastructure/terraform/frontend/main.tf
@@ -62,33 +62,6 @@ resource "aws_s3_object" "frontend" {
   source = "frontend/build/${each.value}"
 }
 
-# Policy to allow public read access to the S3 bucket
-resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
-  bucket = aws_s3_bucket.frontend.bucket
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid: "PublicReadGetObject",
-        Effect = "Allow",
-        Principal: "*",
-        Action: "s3:GetObject",
-        Resource: "arn:aws:s3:::${aws_s3_bucket.frontend.bucket}/*"
-      },
-      {
-        Sid: "AllowCloudFrontAccess",
-        Effect = "Allow",
-        Principal = {
-          AWS: "${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"
-        },
-        Action: "s3:GetObject",
-        Resource: "arn:aws:s3:::${aws_s3_bucket.frontend.bucket}/*"
-      }
-    ]
-  })
-}
-
 # Create SSL Certificate with ACM
 resource "aws_acm_certificate" "frontend" {
   domain_name       = "group-3-frontend-${var.branch_name}.sctp-sandbox.com"
